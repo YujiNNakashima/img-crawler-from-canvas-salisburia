94c81eb6a42d5de88e6beb9e807733c920720740
Yuji <yujiricardo@gmail.com>
2021-10-31 19:40:31 -0300

    feat: add numberOfPages

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs');
 const nodeHtmlToImage = require('node-html-to-image');
 
 (async () => {
 
-  const browser = await puppeteer.launch({headless: true});
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto('http://www.bl.uk/manuscripts/Viewer.aspx?ref=add_ms_18852_fs001r', {
     waitUntil: ['domcontentloaded', 'networkidle0', 'networkidle2'],
   });
 
-  for(let i = 0; i < 5; i++) {
+  const numberOfPages = await page.evaluate(() => {
+    const ul = document.querySelector('#TextDropDown')
+    return ul.children.length
+  })
+
+  for(let i = 0; i < numberOfPages; i++) {
     await page.evaluate(async() => {
       const nextBtn = document.querySelector('#nextpage')
       nextBtn.click()
@@ -27,7 +31,7 @@ const nodeHtmlToImage = require('node-html-to-image');
     nodeHtmlToImage({
       output: `./pages/${i}.png`,
       html: '<html><body><img src="' + imgSrc + '" /></body></html>'
-    }).then(() => console.log('The image was created successfully!'))
+    }).then(() => console.log('imagem criada!'))
 
   }
 
