8943018ba13f0590463868dee048817c7901fda3
Yuji <yujiricardo@gmail.com>
2021-10-31 19:30:16 -0300

    feat: add time

--- index.js
+++ index.js
@@ -4,31 +4,32 @@ const nodeHtmlToImage = require('node-html-to-image');
 
 (async () => {
 
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
   await page.goto('http://www.bl.uk/manuscripts/Viewer.aspx?ref=add_ms_18852_fs001r', {
     waitUntil: ['domcontentloaded', 'networkidle0', 'networkidle2'],
   });
-  // await page.screenshot({ path: 'pages/0.png' });
 
+  for(let i = 0; i < 5; i++) {
+    await page.evaluate(async() => {
+      const nextBtn = document.querySelector('#nextpage')
+      nextBtn.click()
+    })
+    await page.waitForTimeout(3000)
 
-  // for(let i = 1; i < 2; i++) {
-  //   await page.evaluate(async() => {
-  //     const nextBtn = document.querySelector('#nextpage')
-  //     nextBtn.click()
-  //   })
     await page.waitForSelector('canvas')
     const canvas = await page.$('canvas');
     
     const imgSrc = await canvas.evaluate((domElement) => {
       return domElement.toDataURL(); 
-      
     })
 
     nodeHtmlToImage({
-      output: './image.png',
+      output: `./pages/${i}.png`,
       html: '<html><body><img src="' + imgSrc + '" /></body></html>'
     }).then(() => console.log('The image was created successfully!'))
+
+  }
 
   await browser.close();
 })();
